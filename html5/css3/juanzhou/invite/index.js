4dfc15ebd047ef90a5dedfa867950865f3052165
tonny <wodexintiao@gmail.com>
2014-10-15 19:16:55 +0800

    modify

--- html5/css3/juanzhou/invite/index.js
+++ html5/css3/juanzhou/invite/index.js
@@ -1,17 +1,43 @@
 $(function(){
-	var $win = $(window).load(function(){
+	// var $win = $(window).load(function(){
 		var $audio = $('audio');
 		var audio = $audio.get(0);
 		var isCanPlay = false;
+		var isPlaying = false;
 		$audio.on('playing',function(){
-			isCanPlay = true;
+			isPlaying = true;
 		});
-		setTimeout(function(){
-			if(!isCanPlay){
-				alert('点击屏幕播放背景音乐');
+		$audio.on('canplay',function(){
+			isCanPlay = true;
+			setTimeout(function(){
+				if(!isPlaying){
+					alert('点击屏幕播放背景音乐');
+				}
+			},2000);
+			callback();
+		});
+		function preload(img,callback){
+			var image = new Image();
+			image.onload = image.onerror = callback;
+			image.src = img;
+		}
+		var preload_url = ['./img/bg.png','./img/corner.png','./img/wxb.jpg'];
+		var loadingNum = preload_url.length;
+		function preloadCallback(){
+			loadingNum--;
+			if(loadingNum == 0){
+				callback();
 			}
-		},2000);
-		
+		}
+		function callback(){
+			if(isCanPlay && loadingNum == 0){
+				$('.bg_img').addClass('enlarge');
+				run_con_tt = setTimeout(run_con,5000);
+			}
+		}
+		for(var i = 0;i<loadingNum;i++){
+			preload(preload_url[i],preloadCallback);
+		}
 		var run_con_tt;
 		var is_runned = false;
 		function run_con(){
@@ -26,7 +52,7 @@ $(function(){
 					var toHeight = 1250;
 					var $corner = $('.corner');
 					var delay_slide = 3000,
-						delay_hide = 200;
+						delay_hide = 400;
 					var height_corner = $corner.height();
 					$content.animate({
 						height: toHeight - height_corner
@@ -48,10 +74,9 @@ $(function(){
 				},300);
 			});
 		}
-		$('.bg_img').addClass('enlarge');
-		run_con_tt = setTimeout(run_con,5000);
+		
 		$('#main').click(function(){
 			audio.play();
 		});
-	});
+	// });
 });
